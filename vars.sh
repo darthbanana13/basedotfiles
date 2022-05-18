b0971f9af8fdec56cd479fe6c3fcba14d7345907
Radu Margineanu <radu.margineanu@swisscom.com>
2022-05-18 10:48:05 +0200

    Fill template with AWS default cluster values

--- vars.sh
+++ vars.sh
@@ -8,6 +8,9 @@ export AWS_PROXY_HOST=""
 export AWS_PROXY_PORT=""
 export AWS_NOPROXY=""
 
+export AWS_CLUSTER_NAME=
+export AWS_REGION=
+
 export JENKINS_USER_ID=""
 export JENKINS_API_TOKEN=""
 export JENKINS_URL=""
