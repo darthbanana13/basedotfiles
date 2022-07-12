48f7a7e0841b8173553eefcb47adec27434e2dc5
Radu Margineanu <radu.margineanu@swisscom.com>
2022-07-12 08:43:44 +0200

    Move reagent to functions

--- func.sh
+++ func.sh
@@ -129,3 +129,16 @@ wslStartDocker() {
     disown
   fi
 }
+
+#SSH Reagent (http://tychoish.com/post/9-awesome-ssh-tricks/)
+ssh-reagent () {
+  for agent in /tmp/ssh-*/agent.*; do
+    export SSH_AUTH_SOCK=$agent
+      if ssh-add -l 2>&1 > /dev/null; then
+        echo Found working SSH Agent:
+        ssh-add -l
+        return
+      fi
+  done
+  echo Cannot find ssh agent - maybe you should reconnect and forward it?
+}
