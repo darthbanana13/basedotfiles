a45288da05e9711128929f15b4ed0f83ba644308
Radu Margineanu <radu.margineanu@swisscom.com>
2024-02-14 11:03:37 +0100

    Remove Windows workarounds

--- func.sh
+++ func.sh
@@ -122,45 +122,6 @@ wslSetDisplay() {
   export DISPLAY=$("${ipconfig}" | grep -A 5 "vEthernet (WSL)" | "${grepip[@]}"):0.0
 }
 
-# This function is written in a zsh specific way, because arrays/hash maps in zsh are more powerful than bash
-startSingleProcess() {
-  local executableName="${1}"
-  local running=$(ps aux | grep "[${executableName[1,1]}]${executableName[2,-1]}")
-  [[ -n "${running}" ]] && return 0
-  # Probably could be done with disown or setsid, but pdetach is a lot more convenient
-  pdetach sudo -E -H ${@}
-}
-
-startSingleProcessIfExists() {
-  local executableName="${1}"
-  cmdExists "${executableName}" || return 1
-  startSingleProcess ${@}
-}
-
-userStartProcess() {
-  startSingleProcessIfExists ${@} || return 1
-  # disown
-}
-
-# wslBackgroundDocker() {
-  # startSingleProcessIfExists dockerd
-# }
-
-wslStartDocker() {
-  userStartProcess dockerd
-  # dockerd doesn't check if you're running it from an interactive terminal, so it mangles your prompt after
-  # start by setting it to raw, thanks Docker! Undo the setting after starting it.
-  stty -raw
-}
-
-# wslBackgroundK3s() {
-  # startSingleProcessIfExists k3s server
-# }
-
-wslStartK3s() {
-  userStartProcess k3s server
-}
-
 #SSH Reagent (http://tychoish.com/post/9-awesome-ssh-tricks/)
 sshReagent () {
   for agent in /tmp/ssh-*/agent.*; do
